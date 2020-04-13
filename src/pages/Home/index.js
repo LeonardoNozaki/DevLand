f6c38303da5c3f9c75aa8880502054d3c446b3d0
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2020-04-12 23:01:18 -0300

    style: #1 Aplicando regras do eslint nos arquivos existentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,12 @@ import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 
 export default function Logon() {
-
   return (
     <div className="logon-container">
       <section className="form">
-        Voltar <FiLogIn size={16} color="#E02041"/>
+        Voltar
+        <FiLogIn size={16} color="#E02041" />
       </section>
     </div>
-  )
+  );
 }
